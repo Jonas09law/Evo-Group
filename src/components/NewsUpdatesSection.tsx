be17771b51743f10aef93314f8d10c20954256da
Jonas09law <101458317+Jonas09law@users.noreply.github.com>
2025-12-02 23:44:09 +0000

    Update NewsUpdatesSection.tsx

--- src/components/NewsUpdatesSection.tsx
+++ src/components/NewsUpdatesSection.tsx
@@ -3,7 +3,6 @@ import { ChevronLeft, ChevronRight, X } from "lucide-react";
 import { useState } from "react";
 
 const updates = [
-
   {
   date: "28/11/2025",
   title: "UPDATE 2.02",
@@ -24,6 +23,98 @@ Arrumado alguns scripts
 Adicionado tutorial de como resgatar keys`,
 },
 
+  {
+  date: "21/11/2025",
+  title: "UPDATE 2.01",
+  image: "https://media.discordapp.net/attachments/1441545324597608659/1443030621185114223/ATUALIZACAO.png?ex=6930283e&is=692ed6be&hm=809b89ea239ef823d6cdc4cd480ca07958d345556f134612760629d8f4aa8c61&=&format=webp&quality=lossless&width=1006&height=1006",
+  category: "PATCH NOTES",
+  contentText: `
+Arrumado armário da CHOQUE
+Arrumado carro da CHOQUE
+Colocado farda da CHOQUE (Temporaria)
+Arrumado fabricação da Chave
+Arrumado algumas paredes invisíveis 
+Adicionado loja de armas (Pode se comprar coletes etc)
+Adicionado sistema de radio 
+Adicionado nova arma M4A1 RIS
+Adicionado nova arma M16A
+Adicionado Porte de armas
+Arrumado UI mercado
+Arrumado coletes (Agr ja n fica imortal)
+Arrumado parts não ancoradas 
+Novo carro  Lamborghini Galardo
+Arrumado fazenda 
+Arrumado erro ao fabricar a Hi Power 
+Adicionado Turquia
+Agora da para comprar tratamentos na farmácia
+Mais lixos espalhados pela cidade`,
+},
+
+    {
+  date: "20/11/2025",
+  title: "MEGA UPDATE",
+  image: "https://media.discordapp.net/attachments/1441545324597608659/1443030621185114223/ATUALIZACAO.png?ex=6930283e&is=692ed6be&hm=809b89ea239ef823d6cdc4cd480ca07958d345556f134612760629d8f4aa8c61&=&format=webp&quality=lossless&width=1006&height=1006",
+  category: "PATCH NOTES",
+  contentText: `
+Mapa totalmente refeito
+Agora mostra o nick da pessoa quando ela quita do jogo
+Arrumado garagem nao abrir
+Refeito sistema dos carros (gasolina e vida)
+Refeito TODO o sistema de gasolina
+Adicionado sistema de atropelamento
+Arrumado alguns spykes de lag ao entrar no jogo
+Agora ao sair do carro ele freia automaticamente
+Adicionado buzina em todos os carros
+Adicionado burnout
+Adicionado barulho ao ligar o carro 
+Agora quando sai do carro ele freia automaticamente
+Nova UI dos carros
+Agora para ver a hotbar voce ver a hotbar precisa ou abrir o inventario ou apertar TAB
+Agora ao spawnar um carro ele aparecera trancado
+Arrumado erros no driveseat onde dava pra entrar segurando E mesmo trancado
+Agora os plugins e os sistemas dos carros foram unificados finalmente
+Removido o m_steer_camera do R do mouse 
+Agora a foto do menu do jogo voltou a ser a normal
+Adicionado chuva normalmente de novo
+Aidicionado portoes automaticos em locais do mapa
+Arrumado um erro extremamente grave em um module que infelizmente nao posso falar
+Refeito UI da concessionária
+Refeito script do entrar carro que causava MUITO lag
+Agora é possivel usar lockpick mobile
+Não da mais para roubar o próprio carro
+Nao da mais para equipar lockpicks na safe
+Arrumado bug de consguir desespawnar um carro fora da zona se outro ja tiver spawnando
+Agora quando o dono do carro sair do jogo e outra pessoar tiver dirigindo ela pula do carro para impedir o bug do a-chassis
+Alteracoes na tela dos termos
+Terminado emprego de mecanico
+Nova farda PC
+Nova farda PMSP
+Nova farda EB
+Nova farda CHOQUE
+Nova farda PF
+Nova farda PRF
+Novo carro PF
+Novo carro PMSP
+Novo carro ROTA
+Nova farda GRR
+Nova farda COT
+Nova farda ROTA
+Novo carro para os mecanicos
+Nova UI armarios corps/facs
+Arrumado Gari
+Arrumado sistema de fabricar armas 
+Arrumado emprego de fazendeiro 
+Arrumado sistema de GARI
+Nova intro
+Arrumado varios bugs no jogo
+Arrumado rota
+Arrumado mercado
+Arrumado chave
+Arrumado farda PRF
+Arrumado PC
+Otimização no jogo`,
+},
+  
 ];
 
 export default function NewsUpdatesSection() {
@@ -272,5 +363,6 @@ export default function NewsUpdatesSection() {
   );
 
 }
+
 
 
