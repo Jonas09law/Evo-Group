c2c7f429a0b324bb2a05baa08acca806351762c6
Jonas09law <101458317+Jonas09law@users.noreply.github.com>
2025-12-05 17:20:36 +0000

    Update NewsUpdatesSection.tsx

--- src/components/NewsUpdatesSection.tsx
+++ src/components/NewsUpdatesSection.tsx
@@ -2,11 +2,13 @@ import { Card } from "@/components/ui/card";
 import { ChevronLeft, ChevronRight, X } from "lucide-react";
 import { useState } from "react";
 
+import atualizacao from "@/assets/ATUALIZACAO.png";
+
 const updates = [
   {
   date: "28/11/2025",
   title: "UPDATE 2.02",
-  image: "https://media.discordapp.net/attachments/1441545324597608659/1446527913720676464/ATUALIZACAO.png?ex=69344f99&is=6932fe19&hm=4d7c261157957b6f32e497cd68057335d0b23bcdb900a2d17c66eff90cc5c896&=&format=webp&quality=lossless&width=1006&height=1006",
+  image: atualizacao,
   category: "PATCH NOTES",
   contentText: `
 Alterado logo do fluxos no jogo
@@ -26,7 +28,7 @@ Adicionado tutorial de como resgatar keys`,
   {
   date: "21/11/2025",
   title: "UPDATE 2.01",
-  image: "https://media.discordapp.net/attachments/1441545324597608659/1446527913720676464/ATUALIZACAO.png?ex=69344f99&is=6932fe19&hm=4d7c261157957b6f32e497cd68057335d0b23bcdb900a2d17c66eff90cc5c896&=&format=webp&quality=lossless&width=1006&height=1006",
+  image: atualizacao,
   category: "PATCH NOTES",
   contentText: `
 Arrumado armário da CHOQUE
@@ -53,7 +55,7 @@ Mais lixos espalhados pela cidade`,
     {
   date: "20/11/2025",
   title: "MEGA UPDATE",
-  image: "https://media.discordapp.net/attachments/1441545324597608659/1446527913720676464/ATUALIZACAO.png?ex=69344f99&is=6932fe19&hm=4d7c261157957b6f32e497cd68057335d0b23bcdb900a2d17c66eff90cc5c896&=&format=webp&quality=lossless&width=1006&height=1006",
+  image: atualizacao,
   category: "PATCH NOTES",
   contentText: `
 Mapa totalmente refeito
@@ -363,6 +365,7 @@ export default function NewsUpdatesSection() {
   );
 
 }
+
 
 
 
