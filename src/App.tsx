6415aca84b07fc3be265253aa49c8b82cab0f751
Jonas09law <101458317+Jonas09law@users.noreply.github.com>
2025-12-05 16:54:41 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/updates/:slug" element={<UpdatePage />} /> {/* ✅ rota dinâmica */}
+          <Route path="/updates/:slug" element={<UpdatePage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
