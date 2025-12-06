832ac0c9783ddfef9b30c5ce9cbfdc97568cae38
Jonas09law <101458317+Jonas09law@users.noreply.github.com>
2025-12-06 23:06:18 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import Admin from "./components/admin/Admin"; 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { Admin } from "./components/admin/Admin"; // IMPORTA O PAINEL ADMIN AQUI
 
 const queryClient = new QueryClient();
 
